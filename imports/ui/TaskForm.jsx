462475f68778a29cfbfe4ca1e5add282931733af
marlissson silva <marlyssonsilva005@gmail.com>
2023-09-14 07:08:48 -0300

    primeiro commit com o codigo do tutorial ate a parte 6

---
+++ imports/ui/TaskForm.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import { TasksCollection } from '/imports/api/TasksCollection'
+
+export const TaskForm = () => {
+    const [text, setText] = useState("");
+
+    const handleSubmit = e => {
+        e.preventDefault()
+
+        if (!text) return
+
+        TasksCollection.insert({
+            text: text.trim(),
+            createdAt: new Date(),
+        })
+
+        setText("")
+    }
+    return (
+        <form className="task-form" onSubmit={handleSubmit}>
+            <input
+                type="text"
+                placeholder="Digite uma nova tarefa"
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+            />
+
+            <button type="submit">Adicionar tarefa</button>
+        </form>
+    );
+};
